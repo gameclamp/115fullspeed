337e4e38dcca866a77394ad48022a25766dc204b
9尾雪狐 <gameclamp@gmail.com>
2016-04-07 10:51:11 +0800

    Update 115fullspeed.user.js

--- 115fullspeed.user.js
+++ 115fullspeed.user.js
@@ -1,13 +1,15 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        115fullspeed
 // @namespace   https://github.com/gameclamp/115fullspeed
+// @homepageURL https://github.com/gameclamp/115fullspeed
+// @supportURL  https://github.com/gameclamp/115fullspeed/issues
 // @description 批量获取下载地址;调用本地播放器(potplayer)播放原画视频;按文件大小顺序发送下载任务到aria2，更多帮助查看https://github.com/gameclamp/115fullspeed
 // @author      9尾雪狐(gameclamp)
 // @icon        https://github.com/gameclamp/115fullspeed/raw/master/icon.ico
 // @include     http://115.com/?ct=file*
 // @include     http://115.com/?aid=-1&search*
 // @downloadURL https://github.com/gameclamp/115fullspeed/raw/master/115fullspeed.user.js
-// @updateURL   https://github.com/gameclamp/115fullspeed/raw/master/115fullspeed.user.js
+// @updateURL   https://github.com/gameclamp/115fullspeed/raw/master/115fullspeed.meta.js
 // @version     0.3.1
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
