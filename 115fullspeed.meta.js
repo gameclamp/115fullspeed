aaf79aabe8056069f1081c44e2dc7ab2ab2eced3
9尾雪狐 <gameclamp@gmail.com>
2016-04-09 13:44:12 +0800

    Update 115fullspeed.meta.js
    0.3.2

--- 115fullspeed.meta.js
+++ 115fullspeed.meta.js
@@ -10,6 +10,6 @@
 // @include     http://115.com/?aid=-1&search*
 // @downloadURL https://github.com/gameclamp/115fullspeed/raw/master/115fullspeed.user.js
 // @updateURL   https://github.com/gameclamp/115fullspeed/raw/master/115fullspeed.meta.js
-// @version     0.3.1
+// @version     0.3.2
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
